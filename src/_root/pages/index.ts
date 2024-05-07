1ad81026eb52408727413e6319a17a68a4337284
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-05-07 12:37:31 +0300

    chore: create initial project file structure

---
+++ src/_root/pages/index.ts
@@ -0,0 +1,8 @@
+/* This way home will not be imported in one line, it will be grouped
+    e.g. instead of
+     import Home from lalal
+     import aka from lalal
+    it will be import {Home, aka} from lalal
+*/
+
+export { default as Home } from "./Home";
