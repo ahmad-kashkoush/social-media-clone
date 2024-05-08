7a78d33aa3d19b065f3b77cd3486d6c508ac5dcc
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-05-08 15:23:56 +0300

    feat(form): prepare from form shadcn library

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const signupValidation = z.object({
+  name: z.string().min(2, { message: "name should be longer" }),
+  username: z.string().min(2).max(50),
+  email: z.string().email({ message: "invalid email" }),
+  password: z
+    .string()
+    .min(8, { message: "password must be at least 8 characters" }),
+});
