c7f360fc06ffdd74f6590fe5b42218027956f02e
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-06-12 16:22:08 +0300

    feat: create bottombar for small screens

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { bottombarLinks, sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
+import React from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Bottombar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>Bottombar</div>
-  )
-}
+    <section className="bottom-bar">
+      {bottombarLinks.map((link: INavLink) => {
+        const isActive = link.route === pathname;
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`bottombar-link group ${
+              isActive ? "bg-primary-500 rounded-[10px]" : ""
+            } flex-col flex-center gap-1 p-2 transition`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={` ${isActive && "invert-white"}
+                    `}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
 
-export default Bottombar
+export default Bottombar;
