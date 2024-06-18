c43ec3aff834525814bb4587cdb75cd65305b74e
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-06-18 12:45:53 +0300

    feat: add formatDate utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(dateString: string) {
+  const ONE_DAY = 1000 * 60 * 60 * 24;
+  const ONE_HOUR = 1000 * 60 * 60;
+  const ONE_MINUTE = 1000 * 60;
+
+  const inputDate = new Date(dateString);
+  const millisecondsAgo = Date.now() - inputDate.getTime();
+
+  if (millisecondsAgo < ONE_MINUTE) return "Just now";
+  if (millisecondsAgo < ONE_HOUR)
+    return `${Math.floor(millisecondsAgo / ONE_MINUTE)} minutes ago`;
+  if (millisecondsAgo < ONE_DAY)
+    return `${Math.floor(millisecondsAgo / ONE_HOUR)} hours ago`;
+  const daysAgo = Math.floor(millisecondsAgo / ONE_DAY);
+  if (daysAgo < 7) return `${daysAgo} days ago`;
+  if (daysAgo < 30) return `${Math.ceil(daysAgo / 7)} weeks ago`;
+  if (daysAgo < 365) return `${Math.ceil(daysAgo / 30)} months ago`;
+  return `${Math.ceil(daysAgo / 365)} year ago`;
 }
