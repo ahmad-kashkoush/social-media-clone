7059123142bf81a73403159ac35720ecadc58b95
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-06-24 16:26:44 +0300

    feat: create likedPosts component

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,6 +1,22 @@
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUserAccount } from "@/lib/react-query/queriesAndMutations";
 
 export default function LikedPosts() {
+  const { data: currentUser } = useGetCurrentUserAccount();
+  if (!currentUser) {
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+  }
   return (
-    <div>LikedPosts</div>
-  )
+    <>
+      {currentUser.liked.length === 0 && (
+        <p className="text-light-4">No liked posts</p>
+      )}
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
 }
