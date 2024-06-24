1ac135d510dacfcfcc4b2b27b48adbfe02180762
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-06-24 16:27:10 +0300

    feat(GridPostList): update types

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import PostStats from "./PostStats";
 
 type GridPostListProps = {
-  posts?: Models.Document[];
+  posts: Models.Document[];
   showUser?: boolean;
   showStats?: boolean;
 };
@@ -16,8 +16,8 @@ function GridPostList({
   const { user } = useUserContext();
   return (
     <ul className="grid-container">
-      {posts?.map((post: Models.Document) => (
-        <li key={post?.$id} className="relative min-w-80 h-80">
+      {posts.map((post) => (
+        <li key={post?.$id} className="relative">
           <Link to={`/posts/${post?.$id}`} className="grid-post_link">
             <img
               src={post?.imageUrl}
@@ -27,7 +27,7 @@ function GridPostList({
           </Link>
           <div className="grid-post_user">
             {showUser && (
-              <div className="flex items-center justify-start gap-2">
+              <div className="flex items-center justify-start gap-2 flex-1">
                 <img
                   src={post?.creator.imageUrl}
                   className="h-8 w-8 rounded-full"
